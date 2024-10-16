d5267174ce9e1009dc9a3636edf3e6a5c3c0c2d1
Vitor Severo <123137124+Vitor1s@users.noreply.github.com>
2024-10-15 22:58:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,33 +7,3 @@ elementosDuvidas.forEach(function (duvida) {
     duvida.classList.toggle('ativa')
   })
 })
-
-// function nome(argumento) { 
-//     //conteudo da função
-// }
-
-
-
-
-// console.log("Hello, World!");	
-
-// var titulo = document.querySelector(".titulo-pagina");
-// titulo.textContent = "Sistema - Edicursos";
-
-// var traAluno = document.querySelector("#primeiro-aluno");
-
-// var tdNome = traAluno.querySelector(".td-nome");
-// var nome = tdNome.textContent;
-
-// var tdTrabalho = traAluno.querySelector(".td-trabalho");
-// var trabalho = tdTrabalho.textContent;
-
-// var tdProva = traAluno.querySelector(".td-prova");
-// var prova = tdProva.textContent;
-
-// var tdMedia = traAluno.querySelector(".td-media");
-// var media = tdMedia.textContent;
-
-// var mediAluno = trabalho + prova / 2;
-// console.log(mediAluno)
-
